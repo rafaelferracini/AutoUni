0d863d4a393e08dc968a22f9ed7a111dad9d4d6f
Rafael_Ferracini <rafaelhferracini@gmail.com>
2023-03-11 19:43:53 -0300

    Comments

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import datetime
 
 courses = []
 
+# Checa a permissão de admin (necessario para criação do link simbolico)
 def IsAdm():
     if not ctypes.windll.shell32.IsUserAnAdmin():
         print("No admin")
@@ -17,9 +18,9 @@ def IsAdm():
     print("already adm")
     return
 
+# Obtem o semestre atual a partir das datas do calendario academico. Futuramente integrar com o calendario para obter os dados automaticamente
 def get_semester(today):
     # List of semesters [(start, end)]
-    # Automatizar com integração com calendario academico
     semesters = [(datetime.date(2023, 3, 6), datetime.date(2023, 7, 12)),
                  (datetime.date(2023, 8, 7), datetime.date(2023, 12, 16)),
                  (datetime.date(2024, 3, 4), datetime.date(2024, 7, 6)),
@@ -32,21 +33,29 @@ def get_semester(today):
                 return 1
             return 2
 
+# Cria o diretorio correspondente ao atual semestre. Futuramente integrar com o sistema da UFSC para também criar os diretórios das disciplinas
+def directory(year, sem):
+    if not os.path.exists(f"D:/University/{year}.{sem}"):
+        os.makedirs(f"D:/University/{year}.{sem}")
+    
+# Atualiza os cursos do atual semestre a partir das pastas do semestre correspondente
 def att_courses(year, sem):
     for dir in os.listdir(f"D:/University/{year}.{sem}"): # Automatizar processo de identificação de semestre
         courses.append(dir)
 
+# Exibe um menu para selecionar o curso atual que estará utilizando. Futuramente automatizar a seleção a partir da integração com Google Calendar 
 def choose_course():
     print("-----------------------------")
     for course in courses:
-        print(f"[{courses.index(course)}] {course}") # Automatizar criação de pastas para o curso a partir da matricula 
+        print(f"[{courses.index(course)}] {course}") 
     print("[9] Exit")
     print("-----------------------------")
-    current_course = int(input("Selectione o curso atual: "))  # Automatizar processo de curso a partir de integração com Google Calendar
+    current_course = int(input("Selectione o curso atual: "))  
     if current_course == 9:
         exit()
     return current_course
 
+# Cria o link simbolico para o curso atual
 def create_sym(current_course, year, sem):
     if os.path.exists("C:/Users/rafae/current_course"):
         os.unlink("C:/Users/rafae/current_course")
